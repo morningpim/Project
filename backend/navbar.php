6d8153482e752ced8b4d8685275c2e90d56c11ea
Pimpa1 <108038628+Pimpa1@users.noreply.github.com>
2025-04-24 14:53:37 +0700

    first commit

---
+++ backend/navbar.php
@@ -0,0 +1,10 @@
+<!-- navbar.php -->
+<div class="navbar-wrapper">
+  <div class="navbar">
+    <a href="#" class="active">หน้าหลัก</a>
+    <a href="pre.html" class="active">วิชาที่ต้องสอนล่วงหน้า</a>
+    <a href="teacher.html" class="active">ข้อมูลอาจารย์</a>
+    <a href="room.html" class="active">ห้องเรียน</a>
+    <a href="student.html" class="active">กลุ่มเรียนของนักศึกษา</a>
+  </div>
+</div>
